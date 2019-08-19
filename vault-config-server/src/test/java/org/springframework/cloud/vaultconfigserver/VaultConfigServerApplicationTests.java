867157cb3b76af1fee7c77d14a5968ae287d2431
Pankesh Contractor <pankesh_contractor@cable.comcast.com>
2019-08-19 02:30:17 -0400

    getting to work with vault for demo

--- vault-config-server/src/test/java/org/springframework/cloud/vaultconfigserver/VaultConfigServerApplicationTests.java
+++ vault-config-server/src/test/java/org/springframework/cloud/vaultconfigserver/VaultConfigServerApplicationTests.java
@@ -3,10 +3,11 @@ package org.springframework.cloud.vaultconfigserver;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes=VaultConfigServerApplication.class)
 public class VaultConfigServerApplicationTests {
 
 	@Test
